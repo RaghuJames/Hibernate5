f9877d5c94853cd1a353d38cb5a3f41ef9b6ce94
raghu <raghu.a@ncssoft.in>
2017-07-28 20:36:52 +0530

    Commit 2

--- JavaSource/com/hib/web/LoginFormController.java
+++ JavaSource/com/hib/web/LoginFormController.java
@@ -1,5 +1,14 @@
 package com.hib.web;
 
-public class LoginFormController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class LoginFormController {
+	@RequestMapping(value="/login.htm",method=RequestMethod.GET)
+	private String formBack(){
+		System.out.println("===LOGIN===");
+		return "login";
+	}
 }
